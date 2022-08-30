f096d40cfccf9fb665469415afac8059634ca428
Lauren Miller <lauren.amiller559@gmail.com>
2022-08-29 21:54:57 -0700

    Added files to repository

---
+++ Task Organizer/TaskViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TaskViewController.swift
+//  Task Organizer
+//
+//  Created by Lauren Miller on 8/27/22.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var task: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+        guard let currentPosition = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+       guard let count = UserDefaults().value(forKey: "count") as? Int else {
+           return
+        }
+        if count >= 1 {
+        let newCount = count - 1
+        UserDefaults().setValue(newCount, forKey: "count")
+        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+        }
+        
+    }
+    
+
+
+
+}
